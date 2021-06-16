ff2d424ab47d792767d36584d61d96d613ce9704
RanaA1995 <45965464+RanaA1995@users.noreply.github.com>
2021-06-17 00:04:39 +0300

    Update main_code.py

--- applicationPy/main_code.py
+++ applicationPy/main_code.py
@@ -1,1 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello To the new Docker container !!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
